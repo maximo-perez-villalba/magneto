662ae87d97ca981b1e7068a106302583e4cc45d6
Máximo Perez Villalba <mvillalba@socialnet.com.ar>
2022-11-09 15:51:37 -0300

    v0.1.0 spring-boot start

---
+++ src/main/java/com/magneto/analyzer/MagnetoApplication.java
@@ -0,0 +1,13 @@
+package com.magneto.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magneto/analyzer/MagnetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magneto.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
